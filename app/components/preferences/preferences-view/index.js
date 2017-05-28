d75dcd7f9b3a9b93a21f293331cf804d267a375c
Sebastian Prein <hi@sebastianprein.com>
2017-05-28 21:24:23 +0200

    Fix preferences window not showing up

--- app/components/preferences/preferences-view/index.js
+++ app/components/preferences/preferences-view/index.js
@@ -1,4 +1,4 @@
-/* global document, customElements, BaseElement, PreferencesEvent, TabEvent, Electron */
+/* global document, customElements, BaseElement, PreferencesEvent, TabEvent, Electron, requestAnimationFrame */
 
 class PreferencesView extends BaseElement {
     constructor() {
@@ -7,19 +7,23 @@ class PreferencesView extends BaseElement {
         // Register event listener for changes of all preferences items
         PreferencesEvent.on('change', e => this.onChange(e));
 
-        // Since the preferences markup has been created, which means that all
-        // tabs including their event listeners have also been created, we now
-        // can add a final listener to the tab.switch event which will be run
-        // after all tabs have been updated
-        TabEvent.on('switch', ({ detail: { tab } }) => {
-            const currentWindow = Electron.remote.getCurrentWindow();
+        // Register event listener for tab switches
+        TabEvent.on('switch', e => this.onSwitch(e));
 
-            // If the quit tab was clicked, just quit the app
-            if (tab === 'quit') {
-                Electron.remote.app.quit();
-                return;
-            }
+        // Register event listener for clicks within the preferences view
+        this.shadowRoot.addEventListener('click', e => this.onClick(e));
+    }
 
+    /**
+     * Updates the preferences window by gathering the current content size and
+     * showing the window if it’s not visible.
+     *
+     * @return {PreferencesView}
+     */
+    update() {
+        const currentWindow = Electron.remote.getCurrentWindow();
+
+        requestAnimationFrame(() => {
             // Update the window size
             currentWindow.setContentSize(
                 document.body.offsetWidth,
@@ -33,20 +37,7 @@ class PreferencesView extends BaseElement {
             }
         });
 
-        // Open links externally by default
-        document.documentElement.addEventListener('click', (e) => {
-            let target = e.target;
-
-            do {
-                if (target.matches('a[href^="http"]')) {
-                    Electron.shell.openExternal(target.href);
-                    e.preventDefault();
-                    return;
-                }
-
-                target = target.parentElement;
-            } while (target !== null);
-        });
+        return this;
     }
 
     /**
@@ -56,10 +47,47 @@ class PreferencesView extends BaseElement {
      *
      * @param {string} key Preferences item key.
      * @param {string} value Preferences item value.
-     * @return {Preferences}
+     * @return {PreferencesView}
      */
     onChange({ detail: { key, value } }) {
         Electron.ipcRenderer.send('preferences.set', key, value);
+
+        return this;
+    }
+
+    /**
+     * When a tab switched occured, make sure the preferences window is up to
+     * date regarding it’s size. Also specific clicked tabs cause specific
+     * actions.
+     *
+     * @param {TabEvent} e Original emitted tab event.
+     * @return {PreferencesView}
+     */
+    onSwitch({ detail: { tab } }) {
+        if (tab === 'quit') {
+            Electron.remote.app.quit();
+
+            return this;
+        }
+
+        return this.update();
+    }
+
+    /**
+     * A click occured within the preferences view.
+     *
+     * @param {Event} e Original emitted event.
+     * @return {PreferencesView}
+     */
+    onClick(e) {
+        const target = e.target;
+
+        // Make sure links will open externally
+        if (target && target.closest('a[href^="http"]')) {
+            Electron.shell.openExternal(target.href);
+
+            e.preventDefault();
+        }
 
         return this;
     }
