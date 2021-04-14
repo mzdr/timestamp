24220c26b77218f17770e8399491c21da02152ff
Sebastian Prein <hi@sebastianprein.com>
2021-04-15 01:19:36 +0200

    chore: prepare code signing

---
+++ scripts/notarize.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv').config();
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { notarize } = require('electron-notarize');
+const { build } = require('../package.json');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir, packager } = context;
+
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appBundleId = build.appId;
+  const appPath = `${appOutDir}/${packager.appInfo.productFilename}.app`;
+  const appleId = process.env.APPLE_ID;
+  const appleIdPassword = `@keychain:"Application Loader: ${appleId}"`;
+
+  await notarize({
+    appBundleId,
+    appPath,
+    appleId,
+    appleIdPassword,
+  });
+};
