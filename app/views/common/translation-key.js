f8077ae08f3bb9d5c5e5251ef265c6b81a875b25
Sebastian Prein <hi@sebastianprein.com>
2021-12-16 21:53:31 +0100

    refactor(preferences): drop unnecessary customElement and use import map

---
+++ app/views/common/translation-key.js
@@ -0,0 +1,24 @@
+import { dispatch } from '@browserkids/dom';
+
+customElements.define('translation-key', class TranslationKey extends HTMLElement {
+  #key;
+
+  constructor() {
+    super();
+
+    this.#key = this.textContent;
+    this.render();
+  }
+
+  async render() {
+    const { app } = window;
+
+    if (this.hasAttribute('markdown')) {
+      this.innerHTML = await app?.translate(this.#key, { markdown: true });
+    } else {
+      this.textContent = await app?.translate(this.#key);
+    }
+
+    dispatch(this, 'postrender');
+  }
+});
--- app/views/preferences/renderer.js
+++ app/views/preferences/renderer.js
@@ -1,5 +1,98 @@
-import PreferencesView from './preferences-view/preferences-view.js';
-import TranslationKey from '../common/translation-key/translation-key.js';
+import { bindAttributes, bindEventListeners, findReferences } from '@browserkids/dom';
 
-customElements.define('preferences-view', PreferencesView);
-customElements.define('translation-key', TranslationKey);
+window.renderer = new class Renderer {
+  constructor({ app, preferences }) {
+    this.$root = document.documentElement;
+    this.$refs = findReferences(this.$root);
+    this.app = app;
+    this.preferences = preferences;
+
+    bindEventListeners(this.$root, this);
+
+    bindAttributes(this.$root, {
+      productName: this.app.productName,
+      version: this.app.version,
+    });
+
+    this.app.on('update-downloaded', this.onUpdateDownloaded.bind(this));
+    this.render();
+  }
+
+  onCategoryClicked({ currentTarget }) {
+    const { $content, $tab } = this.$refs;
+    const index = $tab.indexOf(currentTarget);
+    const toggleActive = ($el, position) => $el.classList.toggle('-active', index === position);
+
+    [$tab, $content].forEach(($el) => $el.forEach(toggleActive));
+  }
+
+  onInput({ target }) {
+    const {
+      name,
+      value,
+      type,
+      checked,
+    } = target;
+
+    const isBoolean = ['on', 'off'].indexOf(value) >= 0 && ['checkbox', 'radio'].indexOf(type) >= 0;
+
+    this.preferences.set(name, isBoolean ? checked : value);
+  }
+
+  onKeyDown({ key }) {
+    if (key === 'Escape') {
+      this.preferences.hide();
+    }
+  }
+
+  onPostRender() {
+    this.app.resizeWindow({
+      height: this.$root.offsetHeight,
+      width: this.$root.offsetWidth,
+    });
+  }
+
+  onQuitClicked() {
+    this.app.quit();
+  }
+
+  onRestartClicked() {
+    this.app.restart();
+  }
+
+  onUpdateDownloaded() {
+    this.$refs.$alert.classList.remove('-hidden');
+  }
+
+  async render() {
+    const {
+      $backgrounds,
+      $keys,
+      $form,
+      $tab,
+    } = this.$refs;
+
+    const all = await this.preferences.getAll();
+    const backgrounds = await this.preferences.getBackgrounds();
+    const shortcuts = await this.app.translate('preferences.shortcuts.keys');
+
+    backgrounds
+      .map((background) => ([background, background.split('/').pop().split('.').shift()]))
+      .map(([value, name]) => `<option value="${value}">${name}</option>`)
+      .forEach((background) => $backgrounds.insertAdjacentHTML('beforeend', background));
+
+    shortcuts
+      .map(([keys, label]) => ([keys.split('+').map((key) => `<span class="key">${key}</span>`).join('+'), label]))
+      .map(([keys, label]) => `<dt class="keys">${keys}</dt><dd class="label">${label}</dd>`)
+      .forEach((shortcut) => $keys.insertAdjacentHTML('beforeend', shortcut));
+
+    Array
+      .from(all)
+      .filter(([key]) => $form[key])
+      .forEach(([key, value]) => {
+        $form[key][typeof value === 'boolean' ? 'checked' : 'value'] = value;
+      });
+
+    this.onCategoryClicked({ currentTarget: $tab[0] });
+  }
+}(window);
